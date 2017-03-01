c5353851a23a223177a159fe9d6522d884804a59
Ipan Taupik Rahman <ipan.rahman@wgs.co.id>
2017-03-01 13:59:01 +0700

    setup gradle project

---
+++ src/main/java/com/github/ipan97/java/multithreading/Application.java
@@ -0,0 +1,15 @@
+package com.github.ipan97.java.multithreading;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/*
+* @author Ipan Taupik Rahman
+*/
+public class Application {
+	private static Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		log.info("Starting project");
+	}
+}
